1335b1dd68268b1a5067eacff31fafd9f0b7bc38
Kb-tay <taykaiboon12@gmail.com>
2023-10-01 20:29:15 +0800

    Integrate instruction and profile page

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -22,7 +22,7 @@ export default function NavBar() {
         <h1 className="text-2xl mb-5">Dashboard</h1>
         <ul className="space-y-2">
           <li>
-            <Link className="flex items-center gap-3 py-2" href="#">
+            <Link className="flex items-center gap-3 py-2" href="/dashboard">
               <svg
                 className=" h-5 w-5"
                 fill="none"
@@ -42,7 +42,7 @@ export default function NavBar() {
             </Link>
           </li>
           <li>
-            <Link className="flex items-center gap-3 py-2" href="#">
+            <Link className="flex items-center gap-3 py-2" href="/profile">
               <svg
                 className=" h-5 w-5"
                 fill="none"
@@ -60,11 +60,11 @@ export default function NavBar() {
                 <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
                 <path d="M16 3.13a4 4 0 0 1 0 7.75" />
               </svg>
-              <span>Users</span>
+              <span>Profile</span>
             </Link>
           </li>
           <li>
-            <Link className="flex items-center gap-3 py-2" href="#">
+            <Link className="flex items-center gap-3 py-2" href="/instructions">
               <svg
                 className=" h-5 w-5"
                 fill="none"
@@ -80,7 +80,7 @@ export default function NavBar() {
                 <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
                 <circle cx="12" cy="12" r="3" />
               </svg>
-              <span>Settings</span>
+              <span>Instructions</span>
             </Link>
           </li>
           <li>
