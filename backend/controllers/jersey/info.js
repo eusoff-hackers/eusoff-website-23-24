158b6f8fcc06b24e461845440fda56a781601e23
Jeff Lee <jefferson0310@gmail.com>
2023-10-08 22:27:12 +0800

    Fix /jersey/info endpoint.

--- backend/controllers/jersey/info.js
+++ backend/controllers/jersey/info.js
@@ -3,8 +3,7 @@ const MODELS = `../../models`;
 
 const { resBuilder } = require(`${UTILS}/req_handler`);
 const { success, sendStatus } = require(`${UTILS}/req_handler`);
-const { logger } = require(`${UTILS}/logger`);
-const { auth } = require(`${UTILS}/auth`);
+const { logger, logAndThrow } = require(`${UTILS}/logger`);
 const { Bid } = require(`${MODELS}/bid`);
 const { Jersey } = require(`${MODELS}/jersey`);
 const { User } = require(`${MODELS}/user`);
@@ -12,41 +11,67 @@ const { User } = require(`${MODELS}/user`);
 const schema = {
   response: {
     200: resBuilder({
-      jerseyInfo: {
-        type: 'object',
-        additionalProperties: {
-          type: 'array',
-          items: {
-            $ref: 'user',
+      type: 'object',
+      patternProperties: {
+        '^[0-9]{1,2}$': {
+          type: `object`,
+          properties: {
+            Male: {
+              type: `array`,
+              items: {
+                $ref: 'user',
+              },
+              additionalProperties: false,
+            },
+            Female: {
+              type: `array`,
+              items: {
+                $ref: 'user',
+              },
+            },
           },
+          additionalProperties: false,
         },
       },
+      additionalProperties: false,
     }),
   },
 };
 
+async function getJerseyInfo(jersey) {
+  const bidders = await Bid.find({ jersey: jersey._id });
+  const biddersInfo = logAndThrow(
+    await Promise.allSettled(bidders.map((bid) => User.findById(bid.user))),
+  );
+
+  const Male = biddersInfo
+    .filter((bidder) => bidder.gender === `Male`)
+    .map(({ username, points }) => ({
+      username,
+      points,
+    }))
+    .sort((a, b) => b.points - a.points);
+
+  const Female = biddersInfo
+    .filter((bidder) => bidder.gender === `Female`)
+    .map(({ username, points }) => ({
+      username,
+      points,
+    }))
+    .sort((a, b) => b.points - a.points);
+
+  return { Male, Female };
+}
+
 async function handler(req, res) {
   try {
     const jerseys = await Jersey.find(); // Fetch all jerseys
-    const jerseyUserPromises = jerseys.map(async (jersey) => {
-      const jerseyNumber = jersey.number;
-      const bidsForJersey = await Bid.find({ jersey: jersey._id });
-      const userPromises = bidsForJersey.map((bid) => User.findById(bid.user));
-      const biddedUsers = await Promise.all(userPromises);
-      return { jersey: jerseyNumber, users: biddedUsers };
-    });
-
-    const results = await Promise.all(jerseyUserPromises);
-
-    const allJerseyInfo = {};
-    results.forEach(({ jersey, users }) => {
-      allJerseyInfo[jersey] = users;
-      // jersey is an object, users is an array of user objects
-    });
-
-    return await success(res, { allJerseyInfo });
+    const data = logAndThrow(
+      await Promise.allSettled(jerseys.map(getJerseyInfo)),
+    );
+    return await success(res, data);
   } catch (error) {
-    logger.error(`Login error: ${error.message}`, { error });
+    logger.error(`Jersey info error: ${error.message}`, { error });
     return sendStatus(res, 500);
   }
 }
@@ -55,6 +80,5 @@ module.exports = {
   method: `GET`,
   url: `/info`,
   schema,
-  preHandler: auth,
   handler,
 };
