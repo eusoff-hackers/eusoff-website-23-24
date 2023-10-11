2fc5d1a3cea2088bb1a33e2352e95a3eaebb3c3e
Jeff Lee <jefferson0310@gmail.com>
2023-10-12 01:27:19 +0800

    Added addTeams function

--- backend/scripts/uploadData.js
+++ backend/scripts/uploadData.js
@@ -6,13 +6,17 @@ const csv = require('csv-parser');
 const bcrypt = require('bcrypt');
 const xkpasswd = require('xkpasswd');
 const { User } = require(`../models/user`);
+const { Team } = require('../models/team');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-async function run() {
+// Map with name as the key and room number as the value
+const nameToRoomNoMap = {};
+
+async function makeUsers() {
   await mongoose.connect(env.MONGO_URI), console.log(`Connected Atlas.`);
 
   rl.question('Please enter name of csv file: ', (csvFileName) => {
@@ -46,6 +50,8 @@ async function run() {
           password: await generatePassword(roomNo, row['Email']), // Added email in
         };
 
+        nameToRoomNoMap[row['Name Preferred']] = roomNo;
+
         const user = new User(userData);
 
         user
@@ -62,7 +68,66 @@ async function run() {
       });
 
     rl.close();
+    addTeams();
   });
+}
+
+async function addTeams() {
+  const newRl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  newRl.question(
+    'Please enter name of csv file with sports: ',
+    (csvFileName) => {
+      const csvFilePath = '../csv_files/';
+
+      // Map with roomNo as the key and sports array as the value
+      let roomNoToSportsMap = {};
+
+      fs.createReadStream(csvFilePath + csvFileName)
+        .on('error', (err) => {
+          console.error('Error reading the CSV file:', err);
+        })
+        .pipe(csv())
+        .on('data', (row) => {
+          const name = row['NAME OF PLAYER'];
+          const sport = row['Sport'];
+          const roomNo = nameToRoomNoMap[name];
+
+          if (!roomNoToSportsMap[roomNo]) {
+            roomNoToSportsMap[roomNo] = [];
+          }
+          roomNoToSportsMap[roomNo].push(sport);
+        })
+        .on('end', async () => {
+          for (const roomNo in roomNoToSportsMap) {
+            const user = await User.findOne({ username: roomNo });
+
+            for (const sport of roomNoToSportsMap[roomNo]) {
+              const team = await Team.findOne({ name: sport });
+
+              if (team && user) {
+                user.teams.push(team._id);
+              }
+            }
+
+            user
+              .save()
+              .then(() => {
+                console.log(`Updated teams for user: ${roomNo}`);
+              })
+              .catch((err) => {
+                console.error(`Error updating user ${roomNo}:`, err);
+              });
+          }
+          console.log('User data updated with teams.');
+        });
+
+      newRl.close();
+    },
+  );
 }
 
 async function generatePassword(roomNo, email) {
@@ -75,11 +140,11 @@ async function generatePassword(roomNo, email) {
     '../csv_files/sample_user_pass.csv',
     { flags: 'a' },
   );
-  writeStream.write(`"${roomNo}", "${originalPassword}", "${email}"\n`);
+  writeStream.write(`${roomNo},${originalPassword},${email}\n`);
   writeStream.end();
 
   const hashedPw = await bcrypt.hash(originalPassword, SALT_ROUNDS);
   return hashedPw;
 }
 
-run();
+makeUsers();
