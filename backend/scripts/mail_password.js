40b66a6891b0292cef60bd71f330b99c19c16b6e
Jeff Lee <jefferson0310@gmail.com>
2023-10-14 02:36:24 +0800

    Add information on mail_passwrd.

--- backend/scripts/mail_password.js
+++ backend/scripts/mail_password.js
@@ -32,44 +32,45 @@ async function send(mails) {
       await Promise.allSettled(
         mails.map(async ({ username, password, email }) => {
           const template = `<!DOCTYPE html>
-      <html lang="en">
-      
-      <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Account Details</title>
-      </head>
-      
-      <body style="font-family: 'Roboto', Arial, sans-serif; margin: 0; padding: 0; background-color: #f7f7f7; color: #333;">
-      
-          <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f7f7f7">
-              <tr>
-                  <td align="center" valign="top" style="padding: 5% 10px;">
-                      <table width="600" border="0" cellspacing="0" cellpadding="20" bgcolor="#ffffff" style="border-radius: 8px; box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);">
-                          <tr>
-                              <td>
-                                  <h2 style="margin-bottom: 30px; color: #333;">Welcome to Eusoff Jersey Bidding!</h2>
-                                  <p style="margin-bottom: 20px; color: #666; line-height: 1.6;">Here are your account details for the bidding:</p>
-                                  <div style="background-color: #f0f0f0; padding: 15px; border-radius: 5px; margin-bottom: 30px;">
-                                      <p style="margin: 0; color: #666; line-height: 1.6;"><strong>Username:</strong> ${username}</p>
-                                      <p style="margin: 0; color: #666; line-height: 1.6;"><strong>Password:</strong> ${password}</p>
-                                  </div>
-                                  <p style="margin-bottom: 30px; color: #666; line-height: 1.6;">
-                                      <a href="${process.env.WEB_URL}" style="background-color: #333; color: #f7f7f7; padding: 10px 15px; border-radius: 5px; text-decoration: none;">Visit the Website</a>
-                                  </p>
-                                  <p style="margin-bottom: 30px; color: #aaa; line-height: 1.6;">If you are not the intended recipient of this email, please reply to this email immediately.</p>
-                                  <hr style="border: none; border-top: 1px solid #e5e5e5; margin: 20px 0;">
-                                  <p style="margin-bottom: 0; color: #aaa; line-height: 1.6;">Happy Bidding,<br>Eusoff Hackers</p>
-                              </td>
-                          </tr>
-                      </table>
-                  </td>
-              </tr>
-          </table>
-      
-      </body>
-      
-      </html>`;
+          <html lang="en">
+          
+          <head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Account Details</title>
+          </head>
+          
+          <body style="font-family: 'Roboto', Arial, sans-serif; margin: 0; padding: 0; background-color: #f7f7f7; color: #333;">
+          
+              <table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#f7f7f7">
+                  <tr>
+                      <td align="center" valign="top" style="padding: 5% 10px;">
+                          <table width="600" border="0" cellspacing="0" cellpadding="20" bgcolor="#ffffff" style="border-radius: 8px; box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);">
+                              <tr>
+                                  <td>
+                                      <h2 style="margin-bottom: 30px; color: #333;">Welcome to Eusoff Jersey Bidding!</h2>
+                                      <p style="margin-bottom: 20px; color: #666; line-height: 1.6;">Here are your account details for the bidding:</p>
+                                      <div style="background-color: #f0f0f0; padding: 15px; border-radius: 5px; margin-bottom: 30px;">
+                                          <p style="margin: 0; color: #666; line-height: 1.6;"><strong>Username:</strong> ${username}</p>
+                                          <p style="margin: 0; color: #666; line-height: 1.6;"><strong>Password:</strong> ${password}</p>
+                                      </div>
+                                      <p style="margin-bottom: 20px; color: #666; line-height: 1.6;">Stay tuned for further updates regarding your bidding round.</p>
+                                      <p style="margin-bottom: 30px; color: #666; line-height: 1.6;">
+                                          <a href="${process.env.WEB_URL}" style="background-color: #333; color: #f7f7f7; padding: 10px 15px; border-radius: 5px; text-decoration: none;">Visit the Website</a>
+                                      </p>
+                                      <p style="margin-bottom: 30px; color: #aaa; line-height: 1.6;">If you are not the intended recipient of this email, please reply to this email immediately.</p>
+                                      <hr style="border: none; border-top: 1px solid #e5e5e5; margin: 20px 0;">
+                                      <p style="margin-bottom: 0; color: #aaa; line-height: 1.6;">Happy Bidding,<br>Eusoff Hackers</p>
+                                  </td>
+                              </tr>
+                          </table>
+                      </td>
+                  </tr>
+              </table>
+          
+          </body>
+          
+          </html>`;
 
           console.log(`Sending email to: ${username}`);
           const info = await transport.sendMail({
