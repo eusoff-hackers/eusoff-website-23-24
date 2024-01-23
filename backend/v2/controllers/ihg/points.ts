3c6ab3275a2d996c1f2d77d404c146ea29df4649
Juan Carlo Vieri <juancarlovieri@juancarlovieri>
2024-01-23 17:36:58 +0800

    Change point calculation method for IHG.

--- backend/v2/controllers/ihg/points.ts
+++ backend/v2/controllers/ihg/points.ts
@@ -1,15 +1,27 @@
 import { FastifyRequest, FastifyReply, RouteOptions } from 'fastify';
 import { IncomingMessage, Server, ServerResponse } from 'http';
-import { IhgPoint } from '../../models/ihgPoint';
 import { success, resBuilder, sendError } from '../../utils/req_handler';
-import { reportError } from '../../utils/logger';
+import { reportError, logAndThrow } from '../../utils/logger';
+import { IhgPlacement } from '../../models/ihgPlacement';
+import { Hall } from '../../models/hall';
+
+const POINTS_REWARD = {
+  carnival: [0, 1, 2, 3, 4, 5, 6],
+  nonCarnival: [0, 1, 1, 3, 3, 4, 5],
+};
 
 const schema = {
   response: {
     200: resBuilder({
       type: `array`,
       items: {
-        $ref: `ihgPoint`,
+        type: `object`,
+        required: [`hall`, `points`],
+        properties: {
+          hall: { $ref: `hall` },
+          points: { type: `number` },
+        },
+        additionalProperties: false,
       },
     }),
   },
@@ -18,12 +30,32 @@ const schema = {
 async function handler(req: FastifyRequest, res: FastifyReply) {
   const session = req.session.get(`session`)!;
   try {
-    const points = await IhgPoint.find().sort({
-      gold: -1,
-      silver: -1,
-      bronze: -1,
-    });
-    return await success(res, points);
+    const halls = await Hall.find();
+    const ret = logAndThrow(
+      await Promise.allSettled(
+        halls.map(async (h) => {
+          const placements = await IhgPlacement.find({ hall: h._id }).populate(
+            `sport`,
+          );
+
+          if (!placements) return { hall: h, points: 0 };
+
+          let points = 0;
+          placements.forEach((p) => {
+            if (p.sport.isCarnival) {
+              points += POINTS_REWARD.carnival[p.place];
+            } else {
+              points += POINTS_REWARD.nonCarnival[p.place];
+            }
+          });
+
+          return { hall: h, points };
+        }),
+      ),
+      `IHG points calculation`,
+    );
+
+    return await success(res, ret);
   } catch (error) {
     reportError(error, `Ihg points handler error`);
     return sendError(res);
