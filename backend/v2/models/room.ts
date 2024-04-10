f0aeb6a838b8d769ffecd0cbf0d5689010dd1c85
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-04-10 17:26:29 +0800

    Add gender to room.

--- backend/v2/models/room.ts
+++ backend/v2/models/room.ts
@@ -1,12 +1,14 @@
 import { Document, Schema, model } from 'mongoose';
 
 const BLOCKS_LIST = [`A`, `B`, `C`, `D`, `E`];
+const GENDERS = ['MALE', 'FEMALE'];
 
 interface iRoom extends Document {
   block: string;
   number: number;
   capacity: number;
   occupancy: number;
+  allowedGenders: string[];
 }
 
 const rRoom = {
@@ -19,6 +21,7 @@ const rRoom = {
     number: { type: `number` },
     capacity: { type: `number`, minimum: 1, maximum: 2 },
     occupancy: { type: `number`, minimum: 0, maximum: 2 },
+    allowedGenders: { type: `array`, items: { type: `string`, enum: GENDERS } },
   },
 };
 
@@ -27,6 +30,7 @@ const roomSchema = new Schema<iRoom>({
   number: { type: Number, required: true },
   capacity: { type: Number, min: 1, max: 2 },
   occupancy: { type: Number, min: 0, max: 2 },
+  allowedGenders: [{ type: String, enum: GENDERS }],
 });
 
 const Room = model<iRoom>(`Room`, roomSchema);
