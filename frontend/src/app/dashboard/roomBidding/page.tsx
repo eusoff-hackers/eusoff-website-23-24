bde94cfac02e090456ab55c79670236663e4a25e
Kb-tay <taykaiboon12@gmail.com>
2024-04-14 18:09:29 +0800

    Add modal for roombidding

--- frontend/src/app/dashboard/roomBidding/page.tsx
+++ frontend/src/app/dashboard/roomBidding/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
 import React from 'react'
-import NavBar from '../../components/NavBar';
 import { CircularProgress } from '@mui/material';
 import {useEffect, useState} from "react";
 import eusoffLogo from "public/eusoff-logo.png";
 import { HiMenuAlt3 } from "react-icons/hi";
+
+// Room Dialog Imports 
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
 
 export interface Room {
   block: string;
@@ -16,12 +25,47 @@ export interface Room {
 
 const axios = require('axios');
 
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+interface RoomDialogProps {
+  dialogOpen: boolean;
+  // handleDialogOpen: () => void;
+  handleDialogClose: () => void;
+  roomContent: Room;
+}
+
 const RoomBidding: React.FC = () => {
   
   const [loading,setLoading] = useState(true);
   const [roomList, setRoomList] = useState<Room []>([]);
   const [open, setOpen] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [roomSelect, setRoomSelect] = useState<Room>(
+    {
+      block: '',
+      number: 0,
+      capacity: 0,
+      occupancy: 0
+    }
+  );
+  
+  const handleDialogOpen = (room : Room) => {
+    setDialogOpen(true);
+    setRoomSelect(room);
+  };
 
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
+
+  console.log(roomSelect)
 
   useEffect(() => {
     console.log('im called')
@@ -78,11 +122,32 @@ const RoomBidding: React.FC = () => {
         {/*Main Content*/}
         <div className="bg-slate-200 mt-10 w-5/6 shadow-2xl text-3xl font-mono rounded-lg divide-y-5 m-auto grid grid-flow-row md:grid-flow-col gap-0 md:items-center py-10 px-5 mb-3">
 
+          {/*Dialog Box*/}
+          <React.Fragment>
+            <Dialog
+              open={dialogOpen}
+              TransitionComponent={Transition}
+              keepMounted
+              onClose={handleDialogClose}
+            >
+              <DialogTitle>{`Room Number:  ${roomSelect.block}${roomSelect.number}`}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  {`Capacity: ${roomSelect.capacity}`}
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleDialogClose}>Close</Button>
+              </DialogActions>
+            </Dialog>
+          </React.Fragment>
+
           {/*Placeholder image and log in text*/}
           <div className=" bg-slate-200 text-center">
             <h1 className="text-black">
             Available Rooms 
             </h1>
+            
           <div className="flex flexCol">
             {
               roomList!==null && roomList.map((room,index)=>{
@@ -94,9 +159,12 @@ const RoomBidding: React.FC = () => {
                   <div
                   key={index}
                   className="bg-gray-800 h-16 w-16 m-2 flex items-center justify-center text-white font-semibold text-xl cursor-pointer hover:bg-gray-500"
-                >
+                  onClick={() => {
+                    handleDialogOpen(room);
+                  }}
+                  >
                        {block}{number}
-                </div>
+                  </div>
                 )
               })
             }
