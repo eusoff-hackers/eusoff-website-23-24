b5f4b39c19a774768b63d2b37c92053c6b3cf125
Juan Carlo Vieri <juan.c.vieri@gmail.com>
2024-04-17 11:15:14 +0800

    Add blocks.

---
+++ backend/v2/models/roomBlock.ts
@@ -0,0 +1,32 @@
+import { Document, Schema, model } from 'mongoose';
+
+const BLOCKS_LIST = [`A`, `B`, `C`, `D`, `E`];
+
+interface iRoomBlock extends Document {
+  block: string;
+  quota: number;
+}
+
+const rRoomBlock = {
+  $id: `roomBlock`,
+  type: `object`,
+  properties: {
+    block: { type: `string`, enum: BLOCKS_LIST },
+    quota: { type: `number` },
+  },
+};
+
+const roomBlockSchema = new Schema<iRoomBlock>({
+  block: {
+    type: String,
+    required: true,
+    index: 1,
+    enum: BLOCKS_LIST,
+    unique: true,
+  },
+  quota: { type: Number, required: true },
+});
+
+const RoomBlock = model<iRoomBlock>(`RoomBlock`, roomBlockSchema);
+
+export { iRoomBlock, RoomBlock, rRoomBlock };
